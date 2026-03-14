795461eb44b83f7c28469f254a1ef37ab9bc3a56
Nazar Vinnichuk <nazar@vinnich.uk>
2026-03-14 22:22:48 +0200

    Adjust the script.

--- generate.py
+++ generate.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from pathlib import Path
+from shutil import copyfile
 
 from dotenv import load_dotenv
 
@@ -7,24 +9,31 @@ from resources import Prompts
 
 load_dotenv()
 
+now = datetime.now()
+output_directory = Path("generated") / f"{now.hour}-{now.minute}"
+output_directory.mkdir(parents=True)
+
+input_file = Path("input.txt")
+copyfile(input_file, output_directory / "input.txt")
+
 prompts = Prompts()
 
 llm_from_prolog = OpenAILLM(prompts, "gpt-5.4")
 llm_to_prolog = llm_from_prolog
 
-original_text = Path("input.txt").read_text()
+original_text = input_file.read_text()
 
 response = llm_to_prolog.convert_to_prolog(original_text, 1, "medium")
 prolog = response.content
 
 assert prolog
-(Path("generated") / "prolog.txt").write_text(prolog)
+(output_directory / "program.pro").write_text(prolog)
 
 stripped_prolog = ""
 
 for line in prolog.splitlines():
     if "%" not in line:
-        stripped_prolog += line
+        stripped_prolog += line + "\n"
         continue
 
     line = line.split("%")[0].rstrip()
@@ -32,7 +41,7 @@ for line in prolog.splitlines():
     if line:
         stripped_prolog += line + "\n"
 
-(Path("generated") / "stripped_prolog.txt").write_text(stripped_prolog)
+(output_directory / "stripped.pro").write_text(stripped_prolog)
 
 for index in range(3):
     response = llm_from_prolog.convert_from_prolog(
@@ -42,4 +51,4 @@ for index in range(3):
 
     assert generated_text
 
-    (Path("generated") / f"{index + 1}.txt").write_text(generated_text)
+    (output_directory / f"generated_{index + 1}.txt").write_text(generated_text)
